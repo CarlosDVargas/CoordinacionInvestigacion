5ce763012142403cc48d6647e2b055c7707cb462
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.com>
2022-06-19 23:33:06 -0600

    Se agrega cuarta parte de creación de Proyectos

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,9 @@
 class Project < ApplicationRecord
   validates :name, presence: true
   validates :code, presence: true, uniqueness: true
-
-  has_many :investigators, through: :project_investigators
+  
   has_many :project_investigators
+  has_many :investigators, through: :project_investigators
 
   accepts_nested_attributes_for :project_investigators, reject_if: ->(attributes) { attributes["name"].blank? }, allow_destroy: true
 end
