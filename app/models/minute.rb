601da2e7aeaf54b257da80768ce7a71800600170
KevinJSS <103754829+KevinJSS@users.noreply.github.com>
2022-08-11 11:25:30 -0600

    Correcciones en las dependecias de Actas

--- app/models/minute.rb
+++ app/models/minute.rb
@@ -1,5 +1,8 @@
 class Minute < ApplicationRecord
+    validates :number, presence: {message: 'El número del acta es requerido'}, uniqueness: true
+    validates :date, presence: {message: 'Es necesario indicar la fecha del acta'} 
+
     has_one_attached :file
     has_many :articles, dependent: :destroy
-     accepts_nested_attributes_for :articles, allow_destroy: true, reject_if: proc { |attr| attr["number"].blank? }
+    accepts_nested_attributes_for :articles, allow_destroy: true, reject_if: proc { |attr| attr["number"].blank? }
 end
